84630168c0a53faad8e26b2f3095fe79dc5ff90e
Jolterix20 <jayeshvbane@gmail.com>
2022-04-30 09:30:06 +0530

    created card and grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useState } from 'react'
+import Card from './components/Card'
 import './App.css'
 
+const cardImages = [
+	{ src: '/img/helmet-1.png' },
+	{ src: '/img/potion-1.png' },
+	{ src: '/img/ring-1.png' },
+	{ src: '/img/scroll-1.png' },
+	{ src: '/img/shield-1.png' },
+	{ src: '/img/sword-1.png' },
+]
+
 function App() {
+	const [cards, setCards] = useState([])
+	const [turns, setTurns] = useState(0)
+	const [choiceOne, setChoiceOne] = useState(null)
+	const [choiceTwo, setChoiceTwo] = useState(null)
+
+	// Shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }))
+
+		setCards(shuffledCards)
+		setTurns(0)
+	}
+
+	// Handle a choice
+	const handleChoice = (card) => {
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+	}
+
 	return (
 		<div className="App">
 			<h1>Magic Match</h1>
-			<button>New Game</button>
+			<button onClick={shuffleCards}>New Game</button>
+
+			<div className="card-grid">
+				{cards.map((card) => (
+					<Card card={card} handleChoice={handleChoice} />
+				))}
+			</div>
 		</div>
 	)
 }
