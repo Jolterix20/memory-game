afaa75de092cd105e461c2b268c2916fa0500a00
Jolterix20 <jayeshvbane@gmail.com>
2022-04-30 10:38:05 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function App() {
 			.sort(() => Math.random() - 0.5)
 			.map((card) => ({ ...card, id: Math.random() }))
 
+		setChoiceOne(null)
+		setChoiceTwo(null)
 		setCards(shuffledCards)
 		setTurns(0)
 	}
@@ -62,6 +64,11 @@ function App() {
 		setDisabled(false)
 	}
 
+	// Start new game automatically
+	useEffect(() => {
+		shuffleCards()
+	}, [])
+
 	return (
 		<div className="App">
 			<h1>Magic Match</h1>
@@ -82,6 +89,7 @@ function App() {
 					/>
 				))}
 			</div>
+			<p>Turns: {turns}</p>
 		</div>
 	)
 }
